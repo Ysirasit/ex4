bc454bec649860538fc3df7b9f3125f6d374c349
Tanyaluk Sirasitdamrongkun <60143402@g.cmru.ac.th>
2019-07-14 11:56:22 +0700

    before change

---
+++ config/database.js
@@ -0,0 +1,12 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'todo'
+});
+
+connection.connect();
+
+module.exports = connection
+
